50417b13539f2df031824e4e8a86ada5509aec44
kraftdorian <75522596+kraftdorian@users.noreply.github.com>
2022-07-14 14:05:06 +0200

    feat: test Just Nothing vs Nothing

--- __tests__/just.spec.ts
+++ __tests__/just.spec.ts
@@ -1,9 +1,15 @@
-import { Just } from '../src';
+import { Just, Nothing } from '../src';
 
 describe('Just', () => {
   test('valueOf returns the wrapped value', () => {
     const x = 'foo';
     const stringJust = new Just(x);
     expect(stringJust.valueOf()).toEqual(x);
+  });
+
+  test('Just Nothing is not equal to Nothing', () => {
+    const nothingJust = new Just(new Nothing());
+    const nothing = new Nothing();
+    expect(nothingJust.valueOf()).not.toEqual(nothing.valueOf());
   });
 });
