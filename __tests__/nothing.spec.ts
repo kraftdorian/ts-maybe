8ee23faa66b7d1f1ec83848eeca9d49091e7e287
kraftdorian <75522596+kraftdorian@users.noreply.github.com>
2022-07-14 14:05:38 +0200

    refactor: use consistent check of nothing symbol in tests

--- __tests__/nothing.spec.ts
+++ __tests__/nothing.spec.ts
@@ -3,6 +3,6 @@ import { Nothing } from '../src';
 describe('Nothing', () => {
   test('valueOf returns Symbol', () => {
     const nothing = new Nothing();
-    expect(typeof nothing.valueOf() === 'symbol').toEqual(true);
+    expect(typeof nothing.valueOf()).toEqual('symbol');
   });
 });
