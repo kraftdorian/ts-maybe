29b0cedf9d6f5d75c23e77bc22baca7ff30c4639
kraftdorian <75522596+kraftdorian@users.noreply.github.com>
2022-07-14 14:05:08 +0200

    feat: test functor fmap laws

--- __tests__/functor/fmap.spec.ts
+++ __tests__/functor/fmap.spec.ts
@@ -4,6 +4,14 @@ function add1(x: number): number {
   return x + 1;
 }
 
+function multiply2(x: number): number {
+  return x * 2;
+}
+
+function id<A>(value: A): A {
+  return value;
+}
+
 describe('fmap', () => {
   test('returns Nothing if input is Nothing', () => {
     const numberNothing: Maybe<number> = new Nothing();
@@ -19,5 +27,18 @@ describe('fmap', () => {
     const x = 1;
     const numberJust: Maybe<number> = new Just(x);
     expect(fmap(add1, numberJust).valueOf()).toEqual(add1(x));
+  });
+
+  test('fmap id = id', () => {
+    const numberJust: Maybe<number> = new Just(1);
+    expect(fmap(id, numberJust).valueOf()).toEqual(numberJust.valueOf());
+  });
+
+  test('fmap (f . g) = fmap f . fmap g', () => {
+    const x = 1;
+    const numberJust: Maybe<number> = new Just(x);
+    const left = fmap((x) => multiply2(add1(x)), numberJust);
+    const right = fmap(multiply2, fmap(add1, numberJust));
+    expect(left.valueOf()).toEqual(right.valueOf());
   });
 });
